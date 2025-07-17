0cf64a292d1db767d579e12d8367082091398b66
mohamedalmail98 <malmail@aus.edu>
2025-07-17 15:45:55 +0400

    Update CGPA.py

--- CGPA.py
+++ CGPA.py
@@ -28,12 +28,12 @@ st.sidebar.markdown(
     <div style="font-family:sans-serif; line-height:1.1; padding-left:5px;">
         <h3 style="margin-bottom:5px;">📘 Grade Point Legend</h3>
         <ul style="list-style:none; padding-left:0; margin-top:0; margin-bottom:10px; font-size:14px;">
-            <li style="margin-bottom:10px;"><b>A</b> = 4.0</li>
-            <li style="margin-bottom:10px;"><b>B+</b> = 3.5</li>
-            <li style="margin-bottom:10px;"><b>B</b> = 3.0</li>
-            <li style="margin-bottom:10px;"><b>C+</b> = 2.5</li>
-            <li style="margin-bottom:10px;"><b>C</b> = 2.0</li>
-            <li style="margin-bottom:10px;"><b>F</b> = 0.0</li>
+            <li style="margin-bottom:15px;"><b>A</b> = 4.0</li>
+            <li style="margin-bottom:15px;"><b>B+</b> = 3.5</li>
+            <li style="margin-bottom:15px;"><b>B</b> = 3.0</li>
+            <li style="margin-bottom:15px;"><b>C+</b> = 2.5</li>
+            <li style="margin-bottom:15px;"><b>C</b> = 2.0</li>
+            <li style="margin-bottom:15px;"><b>F</b> = 0.0</li>
         </ul>
     </div>
     """,
