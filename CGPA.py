99ac4514f452b62787671c0a7e0cca4a3a275702
mohamedalmail98 <malmail@aus.edu>
2025-07-17 15:54:24 +0400

    Update CGPA.py

--- CGPA.py
+++ CGPA.py
@@ -38,7 +38,7 @@ st.sidebar.markdown(
         </ul>
     </div>
 
-    <hr style="border: 0; height: 2px; background: #DDD; margin: 30px 0 10px 0;">
+    <hr style="border: 0; height: 2px; background: #DDD; margin: 40px 0 10px 0;">
     """,
     unsafe_allow_html=True,
 )
